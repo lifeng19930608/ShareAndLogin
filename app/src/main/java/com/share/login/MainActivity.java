5f893ef0675cf0edcc90f9fd8d1326b155f132c2
lifeng <android_lifeng@sina.com>
2019-08-06 14:12:51 +0800

    添加分享测试代码。

--- app/src/main/java/com/share/login/MainActivity.java
+++ app/src/main/java/com/share/login/MainActivity.java
@@ -22,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 ShareBottomDialog shareBottomDialog = new ShareBottomDialog(MainActivity.this);
+                shareBottomDialog.setText("测试分享内容");
+                shareBottomDialog.setTitle("测试标题");
+                shareBottomDialog.setUrl("http://www.baidu.com");
+                shareBottomDialog.setImageUrl("https://www.baidu.com/link?url=JawS5dWaFZWqH0SIzuR8Tx7Bxg4LuGaZeopZ0ZyX01R9tevhLz-d5Plx9OLYvlk5Whc33DY08j_yXGf6K3ukqMbmJyM3hoXct1vuz-lIVXoTMwvFcCnVLkUB81p5NTHMIqvrNDWyo4FaQxy0b2J7aY291yapfyh90aTXEmVZA9om04m8Vj3xgUasvBFE8hIHACdFVZONHpkTihJ8HExrLQmhBnOlWOzvSqAAnUUcK7SDKJN_yPro3tA6z6oiYQWUS6g1P4N9w_9DUzTsp2jtPeJJruu6nQuV4PkXB4KgRACANJTxDSiZD_oVjyEOjimp4eSTFYVbVohv6sLQiLvhU0m4Y7iIFye_4VdFg49BoU6UUFCmKxqg-YtXkc_vvA-w5EhLtt0fN26_-c2vWmjlzVsZHJSJgvhQ3owPNye76Gb3Yfr2nacgZ9SjQpQzfuU6b_2EM1gk394PzrhNgdT4CaJkJ7b18dsXnYTy_lsiwivxTM3ygNxSEDQuEgmrahePe80cpf1t7RyETn6t3BruYkN5n97r7_LOnrM9HzEAAVAkH5WdzCjSCu5FkFl_PQQIOX4j2dVUHfPu_lDOWEk-MOYT8AHdRne_0jGAxcOhKE5vvzgMA-oLR072t1RwY7X0&wd=&eqid=8aa9fb5c0012bfa8000000045d491a0d");
                 shareBottomDialog.show();
                 // 外层如需处理其它业务逻辑
                 shareBottomDialog.setMobActionListener(new MobActionListener() {
